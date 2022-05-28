402bc10a51b3e3a92525bb982474d6c2821662dd
ashishmaurya089 <ashishmaurya089@gmail.com>
2022-05-28 08:35:48 +0530

    second push 28/05/2022

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.png';
-import './App.css';
+import ExpenseItem from "./components/ExpenseItem";
+function App(props) {
+const expenses =[
+  {
+    id : "1",
+    title : "Toilet Paper",
+    amount : 94.4,
+    date : new Date(2020, 2 , 20),
+  },
+  {
+    id : "2",
+    title : "Toilet Paper 1",
+    amount : 4.4,
+    date : new Date(2000, 8 , 20),
+  }
+]
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Ashish
-        </p>
-        <a
-          className="App-link"
-          href="https://www.google.co.in/"
-          target="_blank"
-        >
-          Google.Com
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hello Ashish</h1>
+      <ExpenseItem  title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>
+      <ExpenseItem  title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/>
+      
+    </>
   );
 }
 
